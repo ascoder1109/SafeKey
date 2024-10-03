5ad3169af17dd4bb65b09da5191acdffbb3780d6
Aditya Singh <as1671109@gmail.com>
2024-10-03 19:50:57 +0530

    initial commit

---
+++ backend/securekey/src/main/kotlin/com/ascoder1109/securekey/SecurekeyApplication.kt
@@ -0,0 +1,11 @@
+package com.ascoder1109.securekey
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurekeyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecurekeyApplication>(*args)
+}
---
+++ backend/securekey/src/test/kotlin/com/ascoder1109/securekey/SecurekeyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ascoder1109.securekey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurekeyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
